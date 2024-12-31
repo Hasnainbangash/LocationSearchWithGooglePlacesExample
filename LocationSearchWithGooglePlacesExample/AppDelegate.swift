f62b5099145304b424f60fae7e637d54410d1a04
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-12-31 12:52:07 +0500

    Configuring our APi key as soon as app launches in th app delegate

--- LocationSearchWithGooglePlacesExample/AppDelegate.swift
+++ LocationSearchWithGooglePlacesExample/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // That will configure our APi key as soon as app launches
+        GooglePlacesManager.shared.setup()
+        
         return true
     }
 
