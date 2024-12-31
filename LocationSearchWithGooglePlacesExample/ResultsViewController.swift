00559ddd08a5801f6aa150929f09a54f50366319
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-12-31 15:04:56 +0500

    Create a table view through the code and sets its basic requirements

--- LocationSearchWithGooglePlacesExample/ResultsViewController.swift
+++ LocationSearchWithGooglePlacesExample/ResultsViewController.swift
@@ -1,29 +1,39 @@
-//
-//  ResultsViewController.swift
-//  LocationSearchWithGooglePlacesExample
-//
-//  Created by Elexoft on 31/12/2024.
-//
-
 import UIKit
 
 class ResultsViewController: UIViewController {
 
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+
+    private var places: [Place] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Additional setup after loading the view
+        view.backgroundColor = .systemBlue
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension ResultsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return places.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = places[indexPath.row].name
+        return cell
     }
-    */
+}
 
+extension ResultsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
